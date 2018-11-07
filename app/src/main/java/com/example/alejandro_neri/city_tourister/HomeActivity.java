814c68063153d0ddefcbbe18b3bee6aa6213d993
Marco Lopez <A01329608@itesm.mx>
2018-11-07 13:53:00 -0600

    Primer commit a mi rama

--- app/src/main/java/com/example/alejandro_neri/city_tourister/HomeActivity.java
+++ app/src/main/java/com/example/alejandro_neri/city_tourister/HomeActivity.java
@@ -1,25 +1,21 @@
 package com.example.alejandro_neri.city_tourister;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
 import android.view.MenuItem;
 
-public class InicioActivity extends AppCompatActivity
+public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_inicio);
+        setContentView(R.layout.activity_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
