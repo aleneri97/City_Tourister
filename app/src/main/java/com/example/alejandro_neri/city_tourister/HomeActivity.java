68c7eaf3d669207acf2943c3f0266cf0ba85d94b
Marco Lopez <A01329608@itesm.mx>
2018-11-09 10:51:08 -0600

    Complete Home screen + Map view + Routes %30 + Boletos %5

--- app/src/main/java/com/example/alejandro_neri/city_tourister/HomeActivity.java
+++ app/src/main/java/com/example/alejandro_neri/city_tourister/HomeActivity.java
@@ -1,14 +1,16 @@
 package com.example.alejandro_neri.city_tourister;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
-import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.synnapps.carouselview.CarouselView;
@@ -18,7 +20,7 @@ public class HomeActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     CarouselView carouselView;
-
+    //Imagenes mostradas en el carrusel
     int[] sampleImages = {R.drawable.puebla_fascinante,R.drawable.cholula_milenaria,R.drawable.casa_enanos};
 
     @Override
@@ -26,24 +28,27 @@ public class HomeActivity extends AppCompatActivity
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        toolbar.setTitle("Inicio");
 
+        //Carrusel
+        carouselView = (CarouselView)findViewById(R.id.carouselView);
+        carouselView.setPageCount(sampleImages.length);
+        carouselView.setImageListener(imageListener);
+
+        //Barra lateral
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
-
-        carouselView = (CarouselView)findViewById(R.id.carouselView);
-        carouselView.setPageCount(sampleImages.length);
-
-        carouselView.setImageListener(imageListener);
     }
 
+    //Barra lateral
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -53,32 +58,44 @@ public class HomeActivity extends AppCompatActivity
             super.onBackPressed();
         }
     }
-
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
+        // Manejo de navegacion de los items con tap
         int id = item.getItemId();
 
-        if (id == R.id.nav_terminosycondiciones) {
-            // Handle the camera action
-        } else if (id == R.id.nav_avisodeprivacidad) {
-
-        } else if (id == R.id.nav_contactanos) {
-
-        } else if (id == R.id.nav_actualizaciones) {
-
-        }
-
+        if (id == R.id.nav_terminosycondiciones) { }
+        else if (id == R.id.nav_avisodeprivacidad) { }
+        else if (id == R.id.nav_contactanos) { }
+        else if (id == R.id.nav_actualizaciones) { }
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
 
+    //Carrusel
     ImageListener imageListener = new ImageListener() {
         @Override
         public void setImageForPosition(int position, ImageView imageView) {
             imageView.setImageResource(sampleImages[position]);
         }
     };
+
+    //Conexion de screen con botones
+    public void ViewHome(View view){
+        Intent intent = new Intent(getApplicationContext(),HomeActivity.class);
+        startActivity(intent);
+    }
+    public void ViewMap(View view){
+        Intent intent = new Intent(getApplicationContext(),MapActivity.class);
+        startActivity(intent);
+    }
+    public void ViewRoutes(View view){
+        Intent intent = new Intent(getApplicationContext(),RoutesActivity.class);
+        startActivity(intent);
+    }
+    public void ViewTickets(View view){
+        Intent intent = new Intent(getApplicationContext(),TicketsActivity.class);
+        startActivity(intent);
+    }
 }
--- app/src/main/java/com/example/alejandro_neri/city_tourister/RoutesActivity.java
+++ app/src/main/java/com/example/alejandro_neri/city_tourister/RoutesActivity.java
@@ -14,24 +14,23 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
+public class RoutesActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_map);
-
-        // Obtiene el SupportMapFragment y notifica cuando el mapa esta listo para ser usado
+        setContentView(R.layout.activity_routes);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
-                .findFragmentById(R.id.map);
+                .findFragmentById(R.id.routes);
         mapFragment.getMapAsync(this);
 
         //Toolbar
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        toolbar.setTitle("Mapa");
+        toolbar.setTitle("Rutas");
     }
 
     //Mapa
@@ -40,17 +39,17 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Marcador en catedral
-        LatLng catedral = new LatLng(19.0431, -98.1983);
-        mMap.addMarker(new MarkerOptions().position(catedral).title("Catedral").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)));
+        LatLng catedral = new LatLng(19.042589, -98.197765);
+        mMap.addMarker(new MarkerOptions().position(catedral).title("Catedral").icon(BitmapDescriptorFactory.fromResource(R.drawable.paradas)));
         // Marcador en el los fuertes
         LatLng los_fuertes = new LatLng(19.056955, -98.184076);
-        mMap.addMarker(new MarkerOptions().position(los_fuertes).title("Los fuertes").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)));
+        mMap.addMarker(new MarkerOptions().position(los_fuertes).title("Los fuertes").icon(BitmapDescriptorFactory.fromResource(R.drawable.paradas)));
 
-        // Marcador en el galeria tesoros de la catedral
-        LatLng galeria_catedral = new LatLng(19.0423, -98.1988);
-        mMap.addMarker(new MarkerOptions().position(galeria_catedral).title("Galería de tesoros de la catedral").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntos_de_interes)));
+        // Marcador en el zocalo
+        LatLng zocalo = new LatLng(19.044138, -98.198246);
+        mMap.addMarker(new MarkerOptions().position(zocalo).title("Zócalo").icon(BitmapDescriptorFactory.fromResource(R.drawable.paradas)));
 
-        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(catedral,16));
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(zocalo,16));
     }
 
     //Conexion de screen con botones
