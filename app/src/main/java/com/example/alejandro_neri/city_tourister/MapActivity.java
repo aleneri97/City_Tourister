ac29e463e2db44edcf7a52ab22583343401cf1f2
Marco <A01329608@itesm.mx>
2018-11-12 14:53:04 -0600

    Info de recomendados/puntos de interes - MAPA

--- app/src/main/java/com/example/alejandro_neri/city_tourister/MapActivity.java
+++ app/src/main/java/com/example/alejandro_neri/city_tourister/MapActivity.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -12,6 +13,7 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapActivity extends AppCompatActivity implements OnMapReadyCallback{
@@ -39,19 +41,97 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Marcador en catedral
-        LatLng catedral = new LatLng(19.0431, -98.1983);
-        mMap.addMarker(new MarkerOptions().position(catedral).title("Catedral").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)));
-        // Marcador en el los fuertes
-        LatLng los_fuertes = new LatLng(19.056955, -98.184076);
-        mMap.addMarker(new MarkerOptions().position(los_fuertes).title("Los fuertes").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)));
+        // Catedral
+        LatLng catedral = new LatLng(19.0429, -98.1983);
+        mMap.addMarker(new MarkerOptions().position(catedral).title("Catedral de Puebla").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("La Catedral Basílica de Puebla, es uno de las más importantes inmuebles del centro histórico declarado patrimonio de la humanidad."));
 
-        // Marcador en el galeria tesoros de la catedral
+        // Fuerte de loreto
+        LatLng fuerte_loreto = new LatLng(19.0579, -98.187);
+        mMap.addMarker(new MarkerOptions().position(fuerte_loreto).title("Fuerte de Loreto").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El fuerte de Loreto es una antigua edificacion militar que se encuentra en la ciudad de Puebla. Originalmente se trataba de una capilla construida en lo alto del cerro Acueyametepec, que fue reconstituida a principios del siglo XIX como fortificacion con finalidad militar."));
+
+        // Museo Amparo
+        LatLng museo_amparo = new LatLng(19.0408, -98.1987);
+        mMap.addMarker(new MarkerOptions().position(museo_amparo).title("Museo Amparo").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El Museo Amparo, es un espacio cultural contemporáneo creado en memoria de Amparo Rugarcía de Espinoza esposa del banquero y filántropo mexicano Manuel Espinosa Yglesias."));
+
+        // Santa Clara
+        LatLng santa_clara = new LatLng(19.0451, -98.192);
+        mMap.addMarker(new MarkerOptions().position(santa_clara).title("Santa Clara").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet(""));
+
+        // Barrio del Artista
+        LatLng barrio_artista = new LatLng(19.0441, -98.1922);
+        mMap.addMarker(new MarkerOptions().position(barrio_artista).title("Barrio del Artista").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El Barrio del Artista es un espacio artístico en la ciudad de Puebla de Zaragoza en el estado de Puebla. Ubicado en lo que se conoce como Plazuela del Torno o del Factor, los artistas trabajan y exponen su obra a la vista de los turistas y ciudadanos que visitan este corredor."));
+
+        // Paseo de San Francisco
+        LatLng paseo_snfrancisco = new LatLng(19.0428, -98.1911);
+        mMap.addMarker(new MarkerOptions().position(paseo_snfrancisco).title("Paseo San Francisco").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet(""));
+
+        // San Francisco
+        LatLng san_francisco = new LatLng(19.045, -98.1893);
+        mMap.addMarker(new MarkerOptions().position(san_francisco).title("San Francisco").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El Ex-convento de San Francisco es el primer convento de la Ciudad de Puebla. Fray Toribio de Benavente, uno de los doce primeros religiosos en arribar a la Nueva España, colocó la primera piedra en 1535. Se ubica en el barrio del Alto, la zona más antigua de la ciudad de Puebla, el barrio indígena."));
+
+        // Gruta de Lourdes
+        LatLng gruta_lourdes = new LatLng(19.0535, -98.1851);
+        mMap.addMarker(new MarkerOptions().position(gruta_lourdes).title("Gruta de Lourdes").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("Este templo religioso está dedicado a la Virgen de Lourdes, la cual es venerada en Francia. Según las visiones de Bernadette Soubirous, la Virgen se apareció en la Gruta de Massabiell, que se ubica a las orillas del río Gave de Pau, a las afueras de la población de Lourdes en 1858."));
+
+        // Museo de Antropologia e Historia
+        LatLng museo_antropologico = new LatLng(19.0562, -98.1827);
+        mMap.addMarker(new MarkerOptions().position(museo_antropologico).title("Museo de Antropologia e Historia").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El Museo Regional de Puebla alberga una colección de extraordinario valor cultural y atractivo estético, integrado por el Museo de la No Intervención Fuerte de Loreto, el Monumento Histórico Fuerte de Guadalupe, el Museo Interactivo Imagina, el Planetario, el Auditorio de la Reforma y el Centro Expositor de Puebla."));
+
+        // Teleférico
+        LatLng teleferico = new LatLng(19.0574, -98.181);
+        mMap.addMarker(new MarkerOptions().position(teleferico).title("Teleférico").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("Está ubicado en la zona de los Fuertes, comunicando el Centro Expositor, cuya estación mide 58 metros de altura, con el monumento a Zaragoza, donde la estación se encuentra a 48 metros sobre el suelo."));
+
+        // Museo de la Evolución
+        LatLng museo_evolucion = new LatLng(19.0578, -98.1816);
+        mMap.addMarker(new MarkerOptions().position(museo_evolucion).title("Museo de la Evolución").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El Museo de la Evolución Puebla abarca desde la historia del cosmos hasta las eras geológicas de la Tierra, pasando por el Big Bang y la creación del ser humano. Es un recinto que se especializa en nuestra evolución y todo lo que nos rodea."));
+
+        // Planetario
+        LatLng planetario = new LatLng(19.057, -98.1816);
+        mMap.addMarker(new MarkerOptions().position(planetario).title("Planetario").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El Planetario simula una estación de servicio planetario en el espacio exterior, el cual ofrece un gran entretenimiento y aprendizaje, a través de talleres permanentes, juegos interactivos, un proyector de estrellas y el Domo OMNIMAX."));
+
+        // Centro Expositor
+        LatLng centro_expositor = new LatLng(19.0586, -98.1811);
+        mMap.addMarker(new MarkerOptions().position(centro_expositor).title("Centro Expositor").icon(BitmapDescriptorFactory.fromResource(R.drawable.recomendados)).snippet("El Centro Expositor y de Convenciones de Puebla es un centro de convenciones, exposiciones y también utilizado como arena multiusos. Está ubicado en la Unidad Cívica 5 de Mayo en la zona de Los Fuertes en la Ciudad de Puebla de Zaragoza capital del Estado de Puebla en México."));
+
+        // Galeria tesoros de la catedral
         LatLng galeria_catedral = new LatLng(19.0423, -98.1988);
-        mMap.addMarker(new MarkerOptions().position(galeria_catedral).title("Galería de tesoros de la catedral").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntos_de_interes)));
+        mMap.addMarker(new MarkerOptions().position(galeria_catedral).title("Galería de tesoros de la catedral").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntos_de_interes)).snippet("La Galería Tesoros de la Catedral de Puebla es un espacio dedicado a la difusión del rico acervo patrimonial que resguarda el obispado; en una participación conjunta del Gobierno del Estado y la Arquidiócesis."));
 
+        // Fuerte Guadalupe
+        LatLng fuerte_guadalupe = new LatLng(19.0525, -98.18);
+        mMap.addMarker(new MarkerOptions().position(fuerte_guadalupe).title("Fuerte Guadalupe").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntos_de_interes)).snippet("El fuerte de Guadalupe es una antigua edificacion militar que se encuentra en la ciudad de Puebla. Originalmente se trataba de una capilla construida en lo alto del cerro Acueyametepec, que fue reconstituida a principios del siglo XIX como fortificacion con finalidad militar."));
+
+        // Monumento a Zaragoza
+        LatLng monumento_zaragoza = new LatLng(19.0605, -98.1846);
+        mMap.addMarker(new MarkerOptions().position(monumento_zaragoza).title("Monumento a Zaragoza").icon(BitmapDescriptorFactory.fromResource(R.drawable.puntos_de_interes)).snippet("Es un mausoleo donde de descansan los restos de uno de los generales más importantes que ha tenido México, ubicado cerca de los Fuertes de Loreto y Guadalupe, donde defendió al país."));
+
+        //Zoom de vista al mapa
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(catedral,16));
+
+        //Botones + y - en el mapa
         mMap.getUiSettings().setZoomControlsEnabled(true);
+
+        if(mMap!=null){
+            mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+                @Override
+                public View getInfoWindow(Marker marker) {
+                    return null;
+                }
+
+                @Override
+                public View getInfoContents(Marker marker) {
+                    View row = getLayoutInflater().inflate(R.layout.dialog_catedral, null);
+                    TextView t1_locality = (TextView)row.findViewById(R.id.locality);
+                    TextView t2_snippet = (TextView)row.findViewById(R.id.snippet);
+
+                    t1_locality.setText(marker.getTitle());
+                    t2_snippet.setText(marker.getSnippet());
+
+                    return row;
+                }
+            });
+        }
+
     }
 
     //Conexion de screen con botones
@@ -71,4 +151,5 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         Intent intent = new Intent(getApplicationContext(),TicketsActivity.class);
         startActivity(intent);
     }
+
 }
